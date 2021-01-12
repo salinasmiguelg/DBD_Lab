046ab49253195259a2e489c4e80aa6ba0feb528e
Miguel Salinas González <miguel.salinas@usach.cl>
2021-01-12 04:31:48 -0300

    creación factorys

---
+++ proyectoDBD/database/factories/Metodo_de_pagoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Metodo_de_pago;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class Metodo_de_pagoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Metodo_de_pago::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ proyectoDBD/database/factories/Proceso_compraFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Proceso_compra;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class Proceso_compraFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Proceso_compra::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
