935554e66d490be523c09c31392100a15b74dd39
Matías Osses <matias.osses@usach>
2021-01-12 03:54:12 -0300

    [Matias]-[correccion de error]

--- proyectoDBD/database/migrations/2021_01_12_044646_create_transaccion_comprobantes_table.php
+++ proyectoDBD/database/migrations/2021_01_12_044646_create_transaccion_comprobantes_table.php
@@ -18,7 +18,7 @@ class CreateTransaccionComprobantesTable extends Migration
             $table->unsignedBigInteger('id_transaccions');
             $table->foreign('id_transaccions')->references('id')->on('transaccions');
             $table->unsignedBigInteger('id_comprobantes');
-            $table->foreign('id_comprobantes')->references('id')->on('coprobantes');
+            $table->foreign('id_comprobantes')->references('id')->on('comprobantes');
             $table->timestamps();
         });
     }
