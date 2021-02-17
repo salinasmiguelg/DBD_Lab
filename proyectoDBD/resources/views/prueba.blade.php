b7af20b804bc082f4de59b0243f03dc091d64713
Benjamin Donoso <benjamin.donoso@usach.cl>
2021-02-17 18:02:51 -0300

    [benjamín]-[creando prototipo de vista]

---
+++ proyectoDBD/resources/views/prueba.blade.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <title>Feriinf</title>
+  </head>
+  <body>
+    <h1>Feriinf</h1>
+    <!-- apartado superior -->
+    <div>
+        <div class = "row color1">
+            <div class = "col button text-center">
+                <button type = "button" class = "btn btn-default margen_up color2" >
+                Iniciar Sesion </button>
+            </div>
+            <div class = "col button ">
+                <button type = "button" class = "btn btn-default margen_up color2" >
+                Registrarse </button>
+            </div>
+        </div>
+    </div>
+    <!-- apartado productos -->
+    <div class="container-fluid">
+        <div class="row text-center">
+            <div class="col-sm border border-dark">
+                Fruta
+            </div>
+            <div class="col-sm border border-dark">
+                Verdura
+            </div>
+            <div class="col-sm border border-dark">
+                Herramientas
+            </div>
+            <div class="col-sm border border-dark">
+                Limpieza
+            </div>
+            <div class="col-sm border border-dark">
+                Cocina
+            </div>
+        </div>
+    </div>
+    <div class = "container-fluid">
+        <div class ="row center">
+        <img src="https://i.ibb.co/L5mx9H3/HKMFWLPZ2-FDXPETCBS4-EG5-GE6-I.jpg" alt="HKMFWLPZ2-FDXPETCBS4-EG5-GE6-I" border="0">
+        </div>
+
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+
+<style>
+    .color1{
+        color:blue;
+    }
+    .color2{
+        color:green;
+    }
+</style>
--- proyectoDBD/routes/web.php
+++ proyectoDBD/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('prueba');
 });
 //cantidad
 Route::get('/cantidad','CantidadController@index');
