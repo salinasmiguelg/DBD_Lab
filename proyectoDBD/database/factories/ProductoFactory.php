94c2613902b45ef3a6dd40008c665d024acff0d5
Matías Osses <matias.osses@usach.cl>
2021-03-07 16:16:03 -0300

    [Matías]-[ajuste en faker]

--- proyectoDBD/database/factories/ProductoFactory.php
+++ proyectoDBD/database/factories/ProductoFactory.php
@@ -28,7 +28,7 @@ class ProductoFactory extends Factory
             'nombreProducto'=>$this->faker->randomElement($array = array ('Papa' , 'Acelga' , 'Lechuga','Tomate','Pala','Escoba')),
             'precioUnitario'=>$this->faker->unique()->numberBetween(1, 10000),
             'stock'=>$this->faker->unique()->numberBetween(1, 100),
-            'categoria'=>$this->faker->word,
+            'categoria'=>$this->faker->randomElement($array = array ('Fruta' , 'Verdura' , 'Articulo de aseo','Mariscos','Ropa','Juguetes')),
             'delete' => $this->faker->randomElement($array = array (false)),
             'id_proceso_compras' => Proceso_compra::all()->random()->id,
             'id_cantidads' => Cantidad::all()->random()->id
